3ae37ee3ec64613ec39fce89ae18556ae541d339
MG Creations <info@mgcreations.co.za>
2020-07-04 06:04:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GetVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString;
+            char ch;
+
+            Console.Write("Enter a letter: ");
+            inputString = Console.ReadLine().ToUpper();
+            ch = Convert.ToChar(inputString);
+
+            while(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+            {
+                Console.WriteLine("{0} is a vowel!", ch);
+
+                Console.Write("Enter a letter: ");
+                inputString = Console.ReadLine().ToUpper();
+                ch = Convert.ToChar(inputString);
+
+            }
+
+            Console.WriteLine("{0} is not a vowel!", ch);
+
+        }
+    }
+}
